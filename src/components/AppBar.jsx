2a251cfaffea4832928fa13b30460996b6b90672
satuso <73895314+satuso@users.noreply.github.com>
2022-01-10 21:13:07 +0200

    10.4

---
+++ src/components/AppBar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Text, View, StyleSheet, Pressable } from 'react-native';
+import Constants from 'expo-constants';
+import theme from '../theme';
+
+const styles = StyleSheet.create({
+  container: {
+    paddingTop: Constants.statusBarHeight,
+    paddingLeft: '10px',
+    backgroundColor: theme.colors.primary,
+    height: '80px',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  text: {
+    color: theme.colors.light,
+    fontWeight: theme.fontWeights.bold,
+    fontSize: theme.fontSizes.subheading,
+  },
+});
+
+const AppBar = () => {
+  return <View style={styles.container}>
+    <Pressable>
+      <Text style={styles.text}>Repositories</Text>
+    </Pressable>
+  </View>;
+};
+
+export default AppBar;
---
+++ src/theme.jsx
@@ -0,0 +1,22 @@
+const theme = {
+  colors: {
+    textPrimary: '#24292e',
+    textSecondary: '#586069',
+    primary: '#24292e',
+    secondary: '#0366d6',
+    light: '#ffffff',
+  },
+  fontSizes: {
+    body: 14,
+    subheading: 20,
+  },
+  fonts: {
+    main: 'System',
+  },
+  fontWeights: {
+    normal: '400',
+    bold: '700',
+  },
+};
+
+export default theme;
