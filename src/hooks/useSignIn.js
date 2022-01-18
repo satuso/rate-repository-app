021dd079c1f5da244c0c8ce6699d3a588a1f3a51
satuso <73895314+satuso@users.noreply.github.com>
2022-01-18 14:55:01 +0200

    10.14

--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,15 +1,21 @@
 import { useMutation } from "@apollo/client";
 import { LOGIN } from '../graphql/mutations';
+import AuthStorage from "../utils/authStorage";
 
 const useSignIn = () => {
   const [mutate, result] = useMutation(LOGIN);
 
   const signIn = async ( { username, password } ) => {
     console.log({ credentials: { username, password }});
-    return await mutate({variables: { credentials: { username, password }}});
-  };
 
-  console.log(result);
+    const result = await mutate({ variables: { credentials: { username, password }}});
+    const { data } = result;
+
+    console.log(data.authorize.accessToken);
+
+    await AuthStorage.setAccessToken(data.authorize.accessToken);
+    return result;
+  };
 
   return [ signIn, result ];
 };
